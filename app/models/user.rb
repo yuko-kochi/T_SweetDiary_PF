5680975e6cecdecee38e5f27c38fd46193e8efd8
yuko-kochi <happy.k165131@gmail.com>
2021-08-03 08:18:16 +0000

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # dependent: :destroy 親モデルを削除する際に、その親モデルに紐づく「子モデル」も一緒に削除できる
+  has_many :posts, dependent: :destroy
 end
