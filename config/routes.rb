1b8c1f359bfdcfbccfb13db9f61018348e1af82a
yuko-kochi <happy.k165131@gmail.com>
2021-08-09 06:52:09 +0000

    [Add]relationship and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  devise_for :users 
   root to: 'homes#top'
   get '/search', to: 'search#search'
   resources :posts do
     resource :likes, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  
   resources :tags, only: [:index, :show]
+  
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers' 
+  end
+  
 end
