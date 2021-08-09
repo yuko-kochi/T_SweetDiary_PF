41789d3defe834f9a5c0f1bcf937d978dc9a27dc
yuko-kochi <happy.k165131@gmail.com>
2021-08-09 07:49:26 +0000

    [Add]relationship JS

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,12 +3,12 @@ class RelationshipsController < ApplicationController
   # ——————フォロー機能を作成・保存・削除する——————
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
   end
 
   #————————フォロー・フォロワー一覧を表示する——————
