05c404615e512b64a7454baab8c8d2e37533864f
yuko-kochi <happy.k165131@gmail.com>
2021-08-27 05:42:53 +0000

    [Add]ChatLayout

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module TSweetdiary
 
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
-
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.time_zone = 'Tokyo'
   end
 end
