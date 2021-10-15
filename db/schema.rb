6148340b967970ceaa6e0936d276b5297c56b608
yuko-kochi <happy.k165131@gmail.com>
2021-10-15 11:54:33 +0000

    [Add]hide

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_052424) do
+ActiveRecord::Schema.define(version: 2021_10_15_104809) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_052424) do
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_valid", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
